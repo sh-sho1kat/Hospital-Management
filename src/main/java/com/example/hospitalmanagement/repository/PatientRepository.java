ea6e8a1e165065e9aea8ca3f020b5004d2fb18e1
sh-sho1kat <shefat.hossen.045@gmail.com>
2026-09-05 04:19:36 +0600

    commit 4

--- src/main/java/com/example/hospitalmanagement/repository/PatientRepository.java
+++ src/main/java/com/example/hospitalmanagement/repository/PatientRepository.java
@@ -1,8 +1,21 @@
 package com.example.hospitalmanagement.repository;
 
 import com.example.hospitalmanagement.entity.Patient;
+import com.example.hospitalmanagement.entity.type.BloodGroupType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
 
+    Patient findByName(String name);
+    Patient findByBirthDate(LocalDate date);
+
+    List<Patient> findByNameOrBirthDate(String name, LocalDate date);
+
+    @Query("SELECT p FROM Patient p Where p.bloodgroup = ?1")
+    List<Patient> findbyBloodGroup(@Param("bloodgroup") BloodGroupType bloodgroup);
 }
--- src/test/java/com/example/hospitalmanagement/PatientTest.java
+++ src/test/java/com/example/hospitalmanagement/PatientTest.java
@@ -1,12 +1,16 @@
 package com.example.hospitalmanagement;
 
 import com.example.hospitalmanagement.entity.Patient;
+import com.example.hospitalmanagement.entity.type.BloodGroupType;
 import com.example.hospitalmanagement.repository.PatientRepository;
 import com.example.hospitalmanagement.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -31,5 +35,32 @@ public class PatientTest {
     public void testTransaction() {
         Patient patient = patientService.getPatientById(1L);
         System.out.println(patient);
+    }
+    @Test
+    public void findbyname()
+    {
+        Patient patient = patientRepository.findByName("Ishita Sen");
+        System.out.println(patient);
+    }
+
+    @Test
+    public void findbybirthdate()
+    {
+        Patient patient = patientRepository.findByBirthDate(LocalDate.parse("1993-10-25"));
+        System.out.println(patient);
+    }
+
+    @Test
+    public void findbynameorbirthdate()
+    {
+        List<Patient> patients = patientRepository.findByNameOrBirthDate("Ishita Sen", LocalDate.of(1993,10,25));
+        System.out.println(patients);
+    }
+
+    @Test
+    public void customquery()
+    {
+        List<Patient> patients = patientRepository.findbyBloodGroup(BloodGroupType.O_POSITIVE);
+        System.out.println(patients);
     }
 }
